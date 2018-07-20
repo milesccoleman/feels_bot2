0cebbcfc5a62f85a8a701af16fb9176bc3cd501a
automatedcommunication <aletheia@aletheias-MacBook-Air.local>
2018-07-20 15:05:36 -0700

    backing up original design

--- Feelsbot.vue
+++ Feelsbot.vue
@@ -1,23 +1,26 @@
 <template>
-
-  <div id="emotions">
-
-      <div class="emotion" id="angry" v-bind:style="{backgroundColor: angry}"></div><br><br><br>
-      <div class="emotion" id="happy" v-bind:style="{backgroundColor: happy}"></div><br><br><br><br>
-      <div class="emotion" id="sad" v-bind:style="{backgroundColor: sad}"></div><br><br><br><br>
-
-    <form v-on:keyup.enter.prevent="eventPlay">
-    <input type="text" v-bind:style="{color: color}" v-model="emotion" placeholder="tell me something"/>
-      <audio ref="colorSoundAngry"><source src='static/angrysound.mp3'></audio>
-      <audio ref="colorSoundHappy"><source src='static/happysound.mp3'></audio>
-      <audio ref="colorSoundSad"><source src='static/sadsound.mp3'></audio>
-      <audio ref="colorSoundFearful"><source src='static/fearfulsound.mp3'></audio>
-      <audio ref="colorSoundDisgusted"><source src='static/disgustedsound.mp3'></audio>
-    </form>
-      <div class="emotion" id="fearful" v-bind:style="{backgroundColor: fearful}"></div><br><br><br>
-      <div class="emotion" id="disgusted" v-bind:style="{backgroundColor: disgusted}"></div><br><br><br><br>
+  <div id="app" v-bind:style="{backgroundColor: color}">
+    <div class="color">
+      <form v-on:keyup.enter.prevent="eventPlay">
+        <input type="text" v-bind:style="{color: color}" v-model="emotion" placeholder="tell me something"/>
+        <audio ref="colorSoundAngry">
+          <source src='static/angrysound.mp3'>
+        </audio>
+        <audio ref="colorSoundHappy">
+          <source src='static/happysound.mp3'>
+        </audio>
+        <audio ref="colorSoundSad">
+          <source src='static/sadsound.mp3'>
+        </audio>
+        <audio ref="colorSoundFearful">
+          <source src='static/fearfulsound.mp3'>
+        </audio>
+        <audio ref="colorSoundDisgusted">
+          <source src='static/disgustedsound.mp3'>
+        </audio>
+      </form>
+    </div>
   </div>
-
 </template>
 
 <script>
@@ -26,34 +29,29 @@ export default {
   data () {
     return {
       color: '',
-      emotion: '',
-      angry: '',
-      happy: '',
-      sad: '',
-      fearful: '',
-      disgusted: ''
+      emotion: ''
     }
   },
   methods: {
     eventPlay: function () {
       if (this.emotion === 'angry') {
-        this.angry = 'red'
+        this.color = 'red'
         this.$refs.colorSoundAngry.play()
       }
       if (this.emotion === 'happy') {
-        this.happy = 'blue'
+        this.color = 'lightblue'
         this.$refs.colorSoundHappy.play()
       }
       if (this.emotion === 'sad') {
-        this.sad = 'purple'
+        this.color = 'purple'
         this.$refs.colorSoundSad.play()
       }
       if (this.emotion === 'fearful') {
-        this.fearful = 'yellow'
+        this.color = 'yellow'
         this.$refs.colorSoundFearful.play()
       }
       if (this.emotion === 'disgusted') {
-        this.disgusted = 'teal'
+        this.color = 'brown'
         this.$refs.colorSoundDisgusted.play()
       }
     }
@@ -64,91 +62,35 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
 @import url("https://fonts.googleapis.com/css?family=Proza+Libre|Fira+Mono");
-
-html {
+html, body {
+  height: 100%;
   margin: 0;
   padding: 0;
   background: black;
 }
-emotions {
-  height: 700px;
-  size: 500px;
-  position: float;
-  color: black;
-  z-index: -1;
-  margin-top: 300px;
+
+#app {
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  margin-top: -10px;
 }
-@-webkit-keyframes angerPulse {
-  from {-webkit-box-shadow: 0 0 9px red; }
-  50% {-webkit-box-shadow: 0 0 36px red; }
-  to {-webkit-box-shadow: 0 0 9px red; }
-}
-#angry {
-  height: 75px;
-  z-index: -1;
-  -webkit-animation-name: angerPulse;
-  -webkit-animation-duration: 3s;
-  -webkit-animation-iteration-count: infinite;
-}
-@-webkit-keyframes happyPulse {
-  from {-webkit-box-shadow: 0 0 9px blue; }
-  50% {-webkit-box-shadow: 0 0 36px blue; }
-  to {-webkit-box-shadow: 0 0 9px blue; }
-}
-#happy {
-  height: 75px;
-  z-index: -1;
-  -webkit-animation-name: happyPulse;
-  -webkit-animation-duration: 1.5s;
-  -webkit-animation-iteration-count: infinite;
-}
-@-webkit-keyframes sadPulse {
-  from {-webkit-box-shadow: 0 0 9px purple; }
-  50% {-webkit-box-shadow: 0 0 36px purple; }
-  to {-webkit-box-shadow: 0 0 9px purple; }
-}
-#sad {
-  height: 75px;
-  z-index: -1;
-  -webkit-animation-name: sadPulse;
-  -webkit-animation-duration: 2s;
-  -webkit-animation-iteration-count: infinite;
-}
-@-webkit-keyframes fearfulPulse {
-  from {-webkit-box-shadow: 0 0 9px yellow; }
-  50% {-webkit-box-shadow: 0 0 36px yellow; }
-  to {-webkit-box-shadow: 0 0 9px yellow; }
-}
-#fearful {
-  height: 75px;
-  z-index: -1;
-  -webkit-animation-name: fearfulPulse;
-  -webkit-animation-duration: 2s;
-  -webkit-animation-iteration-count: infinite;
-}
-@-webkit-keyframes disgustedPulse {
-  from {-webkit-box-shadow: 0 0 9px teal; }
-  50% {-webkit-box-shadow: 0 0 36px teal; }
-  to {-webkit-box-shadow: 0 0 9px teal; }
-}
-#disgusted {
-  height: 75px;
-  z-index: -1;
-  -webkit-animation-name: disgustedPulse;
-  -webkit-animation-duration: 2s;
-  -webkit-animation-iteration-count: infinite;
-}
-input {
+
+#app input {
   padding: 1rem 1rem;
   font-size: 2rem;
   border: 0;
   border-radius: 3px;
   background: #fff;
-  z-index: 1;
+  margin-top: 200px;
 }
-form {
-  margin-top: -100px;
+
+h1 {
+  font-family: 'Proza Libre', sans-serif;
+  color: #fff;
+  font-weight: 300;
 }
+
 input[type="color"] {
   width: 40px;
   border: 1px solid black;
